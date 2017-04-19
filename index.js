03d1c5c09c79869242902eb88ff8ba8e26d7996b
shindakun <steve@shindakun.net>
2017-04-19 19:44:03 +0000

    Fin?

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const multer = require('multer');
+
+let upload = multer({ dest: '/tmp'});
+let app = express();
+
+app.get('/', function (req, res) {
+  res.send(`
+    <p>Submit a file to view its filesize.</p>
+    <form action="/" method="post" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit">
+    </form>
+      `);
+});
+app.post('/', upload.single('file'), function (req, res) {
+  res.json({
+    'size': req.file.size
+  });
+});
+app.listen(process.env.PORT || 3000);
